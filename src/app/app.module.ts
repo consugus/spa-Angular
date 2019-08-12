a7d315131a6b67b63b5c7f3bfe3ceac7afdd6985
Gustavo <consugus@gmail.com>
2019-08-12 20:35:24 -0300

    first routing modifications added, and working fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Routes
+import { APP_ROUTING } from './app.routes';
+
+
+// Services
+
+
+
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+import { Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,7 +26,8 @@ import { AboutComponent } from './components/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
